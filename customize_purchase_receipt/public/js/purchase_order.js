f4e443b9633e410cf725a2b61510344db7357aee
Dux Digitech <dux@srv1215641.hstgr.cloud>
2026-05-12 09:10:38 +0000

    Did Changes For Rate Has Become 0 On Changing Qty

--- customize_purchase_receipt/public/js/purchase_order.js
+++ customize_purchase_receipt/public/js/purchase_order.js
@@ -9,13 +9,21 @@ frappe.ui.form.on('Purchase Order Item', {
     },
 
     qty: function(frm, cdt, cdn) {
-        calculate_po(frm, cdt, cdn);
+
+        setTimeout(() => {
+            calculate_po(frm, cdt, cdn);
+        }, 100);
+
     }
 });
 
 function calculate_po(frm, cdt, cdn) {
 
     let row = locals[cdt][cdn];
+
+    if (!row.custom_rate_without_tax) {
+        return;
+    }
 
     let base = flt(row.custom_rate_without_tax || 0);
     let tax = flt(row.custom_tax_percent || 0);
@@ -38,9 +46,85 @@ function calculate_po(frm, cdt, cdn) {
     // Main ERPNext Rate
     frappe.model.set_value(cdt, cdn, 'rate', final_rate);
 
-    // Force ERP recalculation
     refresh_field("items");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// frappe.ui.form.on('Purchase Order Item', {
+
+//     custom_rate_without_tax: function(frm, cdt, cdn) {
+//         calculate_po(frm, cdt, cdn);
+//     },
+
+//     custom_tax_percent: function(frm, cdt, cdn) {
+//         calculate_po(frm, cdt, cdn);
+//     },
+
+//     qty: function(frm, cdt, cdn) {
+//         calculate_po(frm, cdt, cdn);
+//     }
+// });
+
+// function calculate_po(frm, cdt, cdn) {
+
+//     let row = locals[cdt][cdn];
+
+//     let base = flt(row.custom_rate_without_tax || 0);
+//     let tax = flt(row.custom_tax_percent || 0);
+//     let qty = flt(row.qty || 0);
+
+//     // Tax Amount
+//     let tax_amount = (base * tax) / 100;
+
+//     // Final Rate
+//     let final_rate = base + tax_amount;
+
+//     // Total Tax
+//     let total_tax = tax_amount * qty;
+
+//     // Custom Fields
+//     frappe.model.set_value(cdt, cdn, 'custom_tax_amount', tax_amount);
+
+//     frappe.model.set_value(cdt, cdn, 'custom_total_tax_amount', total_tax);
+
+//     // Main ERPNext Rate
+//     frappe.model.set_value(cdt, cdn, 'rate', final_rate);
+
+//     // Force ERP recalculation
+//     refresh_field("items");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // frappe.ui.form.on('Purchase Order Item', {
--- customize_purchase_receipt/public/js/purchase_receipt.js
+++ customize_purchase_receipt/public/js/purchase_receipt.js
@@ -8,8 +8,12 @@ frappe.ui.form.on('Purchase Receipt Item', {
         calculate_tax_and_rate(frm, cdt, cdn);
     },
 
-    received_qty: function(frm, cdt, cdn) {
-        calculate_tax_and_rate(frm, cdt, cdn);
+    qty: function(frm, cdt, cdn) {
+
+        setTimeout(() => {
+            calculate_tax_and_rate(frm, cdt, cdn);
+        }, 100);
+
     }
 
 });
@@ -19,11 +23,14 @@ function calculate_tax_and_rate(frm, cdt, cdn) {
 
     let row = locals[cdt][cdn];
 
+    if (!row.custom_rate_without_tax) {
+        return;
+    }
+
     let base_rate = flt(row.custom_rate_without_tax || 0);
     let tax_percent = flt(row.custom_tax_percent || 0);
 
-    // Correct qty field
-    let qty = flt(row.received_qty || 0);
+    let qty = flt(row.qty || 0);
 
     // Per item tax
     let tax_amount = (base_rate * tax_percent) / 100;
@@ -38,12 +45,77 @@ function calculate_tax_and_rate(frm, cdt, cdn) {
 
     frappe.model.set_value(cdt, cdn, 'custom_total_tax_amount', total_tax);
 
-    // Main ERPNext Rate
+    // Only set rate
     frappe.model.set_value(cdt, cdn, 'rate', final_rate);
 
-    // Force recalculation
     refresh_field("items");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// frappe.ui.form.on('Purchase Receipt Item', {
+
+//     custom_rate_without_tax: function(frm, cdt, cdn) {
+//         calculate_tax_and_rate(frm, cdt, cdn);
+//     },
+
+//     custom_tax_percent: function(frm, cdt, cdn) {
+//         calculate_tax_and_rate(frm, cdt, cdn);
+//     },
+
+//     received_qty: function(frm, cdt, cdn) {
+//         calculate_tax_and_rate(frm, cdt, cdn);
+//     }
+
+// });
+
+
+// function calculate_tax_and_rate(frm, cdt, cdn) {
+
+//     let row = locals[cdt][cdn];
+
+//     let base_rate = flt(row.custom_rate_without_tax || 0);
+//     let tax_percent = flt(row.custom_tax_percent || 0);
+
+//     // Correct qty field
+//     let qty = flt(row.received_qty || 0);
+
+//     // Per item tax
+//     let tax_amount = (base_rate * tax_percent) / 100;
+
+//     // Final rate
+//     let final_rate = base_rate + tax_amount;
+
+//     // Total tax
+//     let total_tax = tax_amount * qty;
+
+//     frappe.model.set_value(cdt, cdn, 'custom_tax_amount', tax_amount);
+
+//     frappe.model.set_value(cdt, cdn, 'custom_total_tax_amount', total_tax);
+
+//     // Main ERPNext Rate
+//     frappe.model.set_value(cdt, cdn, 'rate', final_rate);
+
+//     // Force recalculation
+//     refresh_field("items");
+// }
 
 
 
